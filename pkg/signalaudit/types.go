d6c6633a1927a524e7250d9cbf8af39d8148830d
Vitaliy Ryumshyn <vr@samebits.com>
2026-03-15 20:10:35 +0100

    feat: add signal audit run loading
    Signed-off-by: Vitaliy Ryumshyn <vr@samebits.com>

--- pkg/signalaudit/types.go
+++ pkg/signalaudit/types.go
@@ -3,6 +3,17 @@ package signalaudit
 // Manifest maps scenario IDs to signal-audit expectations.
 type Manifest map[string]Expectation
 
+// Run is the subset of a run artifact needed for signal auditing.
+type Run struct {
+	RunDir      string
+	ScenarioID  string
+	Model       string
+	Provider    string
+	Signals     []string
+	SignalCounts map[string]int
+	SignalSource string
+}
+
 // Expectation defines which signals a scenario should or should not emit.
 type Expectation struct {
 	PrimarySignal            string   `yaml:"primary_signal"`
